526bbfe9e6f79c024538287498bd4896cf70f4df
Rick Thornbrugh <Rick Thornbrugh>
2016-09-13 11:15:58 -0700

    basic weather app

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiUrl = '&appid=0405ba79b7ff936915c06edae1f90d98';
+
+module.exports = {
+    get: function(place) {
+        return fetch(rootUrl + place + apiUrl, {
+            headers: {
+                // No need for special headers
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        });
+    }
+};
